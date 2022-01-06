ccd2ec218ad6939fab2ac6ceab398c3c9fb7978a
Andrew <atmaybury@protonmail.com>
2022-01-07 10:52:55 +1100

    Added fraidyscrape

--- reader-frontend/src/components/AddSubPanel.js
+++ reader-frontend/src/components/AddSubPanel.js
@@ -3,6 +3,9 @@ import { useDispatch } from 'react-redux'
 import { addNewSub } from '../reducers/subReducer'
 import { useField } from '../hooks/index'
 
+import fraidyscrape from 'fraidyscrape'
+//const scraper = new fraidyscrape()
+
 import styled from '@emotion/styled'
 
 const AddSubPanel = () => {
@@ -16,9 +19,14 @@ const AddSubPanel = () => {
     newSub.reset()
   }
 
+  const fraidy = () => {
+
+  }
+
   return(
     <AddSubForm onSubmit={addSub} id="subscribe-form">
       <input { ...newSub.fields } />
+      <button>TEST</button>
       <button type="submit">Subscribe</button>
     </AddSubForm>
   )
